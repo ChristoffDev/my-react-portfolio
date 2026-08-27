2c9bf3403d8240b5dfb245aa1dd998f679691e7b
Christopher Reguya Perez <chrperez@proweaver.net>
2026-08-27 17:10:25 +0800

    added hover background on the LinkButton

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -10,10 +10,10 @@ function LinkButton({ children, href, variant }: LinkButtonProps) {
     return (
         <a href={href}
         className={
-            "px-5 py-2.5 rounded-md " +
-            (variant==="primary" 
-                ? "bg-accent text-background" 
-                : "text-foreground")
+            "px-5 py-2.5 rounded-md transition-colors focus:ring-2 focus:ring-accent " +
+            (variant === "primary" 
+                ? "bg-accent text-background hover:bg-accent/80" 
+                : "border border-foreground/30 text-foreground hover:text-accent hover:border-accent/70")
             }>
             {children}
         </a>
