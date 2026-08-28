7bd713f1dd6b04a72e3301319211abed8b3bbaa1
Christopher Reguya Perez <chrperez@proweaver.net>
2026-08-28 19:01:38 +0800

    added Navbar burger

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,32 +1,53 @@
+import { useState } from "react";
 
 
 function Navbar() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    
     return (
-        <nav className="px-6 py-4 mx-auto max-w-4xl flex justify-between items-center">
-            <div className="font-semibold">
-                Christopher Perez
+        <nav className="px-6 py-4 mx-auto max-w-4xl">
+
+            <div className="flex justify-between items-center">
+
+                <div className="font-semibold">
+                    Christopher Perez
+                </div>
+                
+                <div className="hidden md:flex gap-5 ">
+                    <a href="#about" className="text-muted transition-colors hover:text-accent"
+                    >
+                        About
+                    </a>
+                    <a href="#stack" className="text-muted transition-colors hover:text-accent"
+                    >
+                        Tech Stack
+                    </a>
+                    <a href="#projects" className="text-muted transition-colors hover:text-accent"
+                    >
+                        Projects
+                    </a>
+                    <a href="#contact" className="text-muted transition-colors hover:text-accent">
+                        Contact
+                    </a>
+            
+                </div>
+
+                <button className="block md:hidden" onClick={() => setMenuOpen(!menuOpen)} aria-label={menuOpen ? "Close Menu" : "Open Menu"}>
+                        {menuOpen ? "✕" : "☰"}
+                </button>           
+                        
             </div>
 
-            <div className="hidden md:flex gap-5 ">
-                <a href="#about" className="text-muted transition-colors hover:text-accent"
-                >
-                    About
-                </a>
+            {menuOpen && (
+                        <div className="flex flex-col gap-4 mt-4">
+                            <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
+                            <a href="#stack" onClick={() => setMenuOpen(false)}>Tech Stack</a>
+                            <a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a>
+                            <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
+                        </div>
+                    )}
 
-                <a href="#stack" className="text-muted transition-colors hover:text-accent"
-                >
-                    Tech Stack
-                </a>
-
-                <a href="#projects" className="text-muted transition-colors hover:text-accent"
-                >
-                    Projects
-                </a>
-
-                <a href="#contact" className="text-muted transition-colors hover:text-accent">
-                    Contact
-                </a>
-            </div>
         </nav>
     )
 };
